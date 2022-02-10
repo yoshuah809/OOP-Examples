3c0adad2ef6fecdf97f44dcdd489b6bef9e8f04a
unknown <yoshuah809@gmail.com>
2022-02-09 20:47:14 -0600

    created getaccount history method to print all transactions

--- BankAccount.cs
+++ BankAccount.cs
@@ -30,17 +30,48 @@ namespace OOPExamples
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
-            this.Balance = initialBalance;
+            MakeDeposit(initialBalance, DateTime.Now, "This is The initial Balance");
 
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
         }
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\t\tNote");
+            foreach (var item in allTransactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t\t{item.Notes}");
+            }
+
+            return report.ToString();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -41,7 +41,20 @@ namespace OOPExamples
         {
             var account = new BankAccount("Josue", 10000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+            account.MakeWithdrawal(250,DateTime.Today,"Purchase of Game Console");
+            account.MakeWithdrawal(1250, DateTime.Today, "Purchase of 57in TV");
+            Console.WriteLine($"The Total Account Balance is {account.Balance}");
 
+            account.MakeDeposit(200,DateTime.Today,"Making a Deposit");
+
+            // Test for a negative balance.
+            
+
+            account.MakeWithdrawal(800, DateTime.Today, "Purchase of Wireless Radio");
+            Console.WriteLine($"The Total Account Balance is {account.Balance}");
+
+            
+            Console.WriteLine(account.GetAccountHistory());
 
         }
 
