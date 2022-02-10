0b76b527930fa8c762956b7b17ff63ef3a49e320
unknown <yoshuah809@gmail.com>
2022-02-09 20:25:09 -0600

    added transaction class and its implementation

--- BankAccount.cs
+++ BankAccount.cs
@@ -10,12 +10,30 @@ namespace OOPExamples
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
 
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
             this.Balance = initialBalance;
+
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
